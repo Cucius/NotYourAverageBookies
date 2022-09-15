89e77b19df57c51f7ab45a1da1e6085947697b4d
Cucius <Rylan19@Gmail.com>
2022-09-15 12:21:13 -0600

    Added images and seeds for Biographies

---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+
+INSERT INTO Books (title, authorFirstName, authorLastName, genre, coverIMG)
+VALUES
+('The Happiest Man on Earth','Eddie','Jaku', 'biography', LOAD_FILE('./public/images/biography/Happiest_Man.jpg') ),
+("Can't Hurt Me: Master Your Mind and Defy the Odds",'David','Goggins', 'biography', LOAD_FILE('./public/images/biography/Cant_Hurt_Me.jpg') ),
+('Revenge: Meghan, Harry and the War between the Windsors','Tom','Bower', 'biography', LOAD_FILE('./public/images/biography/Revenge.jpg') ),
+('Rogues: True Stories of Grifters, Killers, Rebels and Crooks','Patrick Radden','Keefe', 'biography', LOAD_FILE('./public/images/biography/Rogues.jpg') ),
+('Vanderbilt: The Rise and Fall of an American Dynasty','Anderson','Cooper', 'biography', LOAD_FILE('./public/images/biography/Vanderbilt.jpg') );
+
+
