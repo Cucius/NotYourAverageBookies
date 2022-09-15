9365decb17ac0f46022597e73b6b735a5cc8c126
Cucius <Rylan19@Gmail.com>
2022-09-15 13:03:45 -0600

    Added images and seeds for Horror

--- db/seeds.sql
+++ db/seeds.sql
@@ -8,9 +8,15 @@ VALUES
 ('Rogues: True Stories of Grifters, Killers, Rebels and Crooks','Patrick Radden','Keefe', 'biography', LOAD_FILE('./public/images/biography/Rogues.jpg') ),
 ('Vanderbilt: The Rise and Fall of an American Dynasty','Anderson','Cooper', 'biography', LOAD_FILE('./public/images/biography/Vanderbilt.jpg') ),
 -- Fantasy
-('Circe','Madeline','Miller', 'fantasy', LOAD_FILE('./public/images/biography/Circe.jpg') ),
-('The Final Gambit','Jennifer Lynn','Barnes', 'fantasy', LOAD_FILE('./public/images/biography/Final_Gambit.jpg') ),
-('The Measure','Nikki','Erlick', 'fantasy', LOAD_FILE('./public/images/biography/Measure.jpg') ),
-('This Time Tomorrow','Emma','Straub', 'fantasy', LOAD_FILE('./public/images/biography/Time_Tomorrow.jpg') ),
-('The Wicked King','Holly','Black', 'fantasy', LOAD_FILE('./public/images/biography/Wicked_King.jpg') );
+('Circe','Madeline','Miller', 'fantasy', LOAD_FILE('./public/fantasy/biography/Circe.jpg') ),
+('The Final Gambit','Jennifer Lynn','Barnes', 'fantasy', LOAD_FILE('./public/images/fantasy/Final_Gambit.jpg') ),
+('The Measure','Nikki','Erlick', 'fantasy', LOAD_FILE('./public/images/fantasy/Measure.jpg') ),
+('This Time Tomorrow','Emma','Straub', 'fantasy', LOAD_FILE('./public/images/fantasy/Time_Tomorrow.jpg') ),
+('The Wicked King','Holly','Black', 'fantasy', LOAD_FILE('./public/images/fantasy/Wicked_King.jpg') ),
+-- Horror
+('The Push','Ashley','Audrain', 'horror', LOAD_FILE('./public/images/horror/Push.jpg') ),
+('One By One','Freida','McFadden', 'horror', LOAD_FILE('./public/images/horror/One_by_one.jpg') ),
+('Nettle & Bone','T.','Kingfisher', 'horror', LOAD_FILE('./public/images/horror/Nettle_Bone.jpg') ),
+('The Last Graduate','Naomi','Novik', 'horror', LOAD_FILE('./public/images/horror/Graduate.jpg') ),
+('The Hive','Gregg','Olsen', 'horror', LOAD_FILE('./public/images/horror/Hive.jpg') );
 
