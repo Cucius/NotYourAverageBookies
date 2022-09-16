70efd703723d5258c21ff1b8a8175e296177070d
myang5t3r <matthew.yang@hunterdouglas.com>
2022-09-16 11:47:09 -0600

    Completed route for home/genre and tested!

---
+++ utils/helpers.js
@@ -0,0 +1,26 @@
+function randomizer(data){
+    // console.log(data)
+    const randomBooks = [];
+        let lastIndex = Math.floor(Math.random()*data.length);
+        for (let i = 0; i< 3 ; i++){
+            let arrayIndex = Math.floor(Math.random()*data.length); 
+            while (arrayIndex === lastIndex){
+                arrayIndex = Math.floor(Math.random()*data.length); 
+            }
+            randomBooks.push(data[arrayIndex]);
+            lastIndex = arrayIndex;
+            // if (arrayIndex === lastIndex){
+            //     arrayIndex = Math.floor(Math.random()*data.length);
+            // } else {
+            //     randomBooks.push(data[arrayIndex]);
+            //     lastIndex = arrayIndex;
+            // }
+        };
+    // console.log(randomBooks)    
+    return randomBooks;    
+};
+
+
+module.exports = randomizer;
+
+
