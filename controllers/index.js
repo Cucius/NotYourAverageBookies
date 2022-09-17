6b0b9faab573a5d179f054e129a8dbfeea1b1b95
myang5t3r <matthew.yang@hunterdouglas.com>
2022-09-17 12:35:48 -0600

    Completed login route, login form, added place holder for logout

--- controllers/index.js
+++ controllers/index.js
@@ -2,8 +2,10 @@ const router = require('express').Router();
 
 const homeRoutes = require('./home-routes');
 const aboutRoutes = require('./about-routes');
+const loginRoutes = require('./login-routes');
 
 router.use('/', homeRoutes);
 router.use('/about', aboutRoutes);
+router.use('/login', loginRoutes);
 
 module.exports = router;
