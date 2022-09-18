ca47dcbfd6f2d291b5ade28e108222015d56164d
Cucius <Rylan19@Gmail.com>
2022-09-18 04:56:34 -0600

    Added code for genrepage tests

---
+++ controllers/api/login-routes.js
@@ -0,0 +1,12 @@
+const router = require("express").Router();
+
+// Get home page - main layout
+router.get("/", async (req, res) => {
+  try {
+    res.render("loginpage");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
