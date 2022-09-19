e59083460c415fda2a7d12a6cf220a2ac83a5bc2
myang5t3r <matthew.yang@hunterdouglas.com>
2022-09-19 11:14:14 -0600

    Fixed bug, needed unique ids for event handlers.

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -1,28 +1,28 @@
 // Get element by class
-btn1 = $('#btn1');
-btn2 = $('#btn2');
-btn3 = $('#btn3');
+homebtn1 = $('#homebtn1');
+homebtn2 = $('#homebtn2');
+homebtn3 = $('#homebtn3');
 // Get element by id
-id1 = $('#id1');
-id2 = $('#id2');
-id3 = $('#id3');
+homeid1 = $('#homeid1');
+homeid2 = $('#homeid2');
+homeid3 = $('#homeid3');
 
 // Function
 
 
 // Event handler
-btn1.on('click', function(e){
+homebtn1.on('click', function(e){
     e.preventDefault();
     // console.log(e.target)
-    console.log(id1.text());
+    console.log(homeid1.text());
 })
-btn2.on('click', function(e){
+homebtn2.on('click', function(e){
     e.preventDefault();
     // console.log(e.target)
-    console.log(id2.text());
+    console.log(homeid2.text());
 })
-btn3.on('click', function(e){
+homebtn3.on('click', function(e){
     e.preventDefault();
     // console.log(e.target)
-    console.log(id3.text());
+    console.log(homeid3.text());
 })
